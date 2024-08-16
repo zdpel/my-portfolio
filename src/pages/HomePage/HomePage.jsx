1b9c1dd4c933809fe8a09d6b3ed23146bfbce037
zdpel <zdpelham@gmail.com>
2024-08-16 17:36:02 -0500

    ghpage test

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the homepage of my portfolio website.</p>
+        </div>
+    );
+};
+
+export default HomePage;
